16f0bf557be5043ed50e1802e05df7d6b6a3a034
rakicibaris97-jpg <rakicibaris97@gmail.com>
2026-05-06 08:03:31 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const admin = require("firebase-admin");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const serviceAccount = require("./firebase-key.json");
+const serviceAccount = const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
